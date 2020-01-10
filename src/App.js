64c14c7daec5ff2ece8f22599e0624bcd0be6966
emphillips <matt-phillips@live.com>
2020-01-09 19:20:25 -0600

    Punk API React App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      beers:[],
+    };
+  }
+
+
+componentDidMount() {
+  fetch('https://api.punkapi.com/v2/beers')
+  .then(results => {
+    return results.json();
+
+  }).then(data => {
+    let beerList = data.results.map(() => {
+      return(
+        <div>
+        </div>
+      )
+    })
+  
+
+    render() {
+      <div>
+        { this.state.beers.map( beer => (
+          <BeerCard beerInfo={beer} />
+        ))}
+      </div>
+    }
+
+  }
+      this.setState({beers: beers});
+      console.log("state", this.state.beers)
+    
+    )
+  }
+
+  button onClick=
+
+// render() {
+//   return (
+//     <div className="container2">
+//       <div className="container1">
+//       </div>
+//     </div>
+//   )
+// }
+
+
+
+
+
 
 export default App;
